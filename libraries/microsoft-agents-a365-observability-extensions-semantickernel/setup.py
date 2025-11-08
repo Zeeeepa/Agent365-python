577cc911746faa3801375c4dd2e5ba02a2094282
Rahul Devikar <radevika@microsoft.com>
2025-11-07 18:05:23 -0800

    Revert "Use same versioning for monorepo"
    This reverts commit 7e735c4a482eeac84ed69d0e551315e9935ea01b.

--- libraries/microsoft-agents-a365-observability-extensions-semantickernel/setup.py
+++ libraries/microsoft-agents-a365-observability-extensions-semantickernel/setup.py
@@ -1,17 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import sys
-from pathlib import Path
+from os import environ
 from setuptools import setup
 
-# Add versioning helper to path
-helper_path = Path(__file__).parent.parent.parent / "versioning" / "helper"
-sys.path.insert(0, str(helper_path))
-
-from setup_utils import get_package_version, get_dynamic_dependencies
+# Get version from environment variable set by CI/CD
+# This will be set by setuptools-git-versioning in the CI pipeline
+package_version = environ.get("AGENT365_PYTHON_SDK_PACKAGE_VERSION", "0.0.0")
 
 setup(
-    version=get_package_version(),
-    install_requires=get_dynamic_dependencies(),
+    version=package_version,
 )
--- versioning/helper/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-"""Agent365 Python SDK setup utilities."""
-
-from .setup_utils import get_package_version, get_dynamic_dependencies
-
-__all__ = ["get_package_version", "get_dynamic_dependencies"]
